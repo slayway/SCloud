9dd0f6a2431467f6794b24a1257adf1872df868a
slayway <Relocation1337@gmail.com>
2018-09-19 07:20:47 +0300

    add OnCLick in RegisterActivity

--- Software/app/src/main/java/com/slayway/slaycloud/RegisterActivity.java
+++ Software/app/src/main/java/com/slayway/slaycloud/RegisterActivity.java
@@ -2,12 +2,40 @@ package com.slayway.slaycloud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button creaateAcc;
+    EditText etFirstName;
+    EditText etLastName;
+    EditText etEmail;
+    EditText etLogin;
+    EditText etPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        creaateAcc = (Button) findViewById(R.id.buttonCreateAcc);
+        etFirstName = (EditText) findViewById(R.id.editTextFirstName);
+        etLastName = (EditText) findViewById(R.id.editTextLastName);
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
+        etLogin = (EditText) findViewById(R.id.editTextLogin);
+        etPass = (EditText) findViewById(R.id.editTextPass);
+
+        creaateAcc.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonCreateAcc:
+                break;
+        }
     }
 }
