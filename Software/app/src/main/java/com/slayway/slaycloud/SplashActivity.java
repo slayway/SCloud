c22a3e23feae3fb5ce5aed4345cc5509763f9d0a
slayway <Relocation1337@gmail.com>
2018-09-19 07:01:35 +0300

    create splash activity

---
+++ Software/app/src/main/java/com/slayway/slaycloud/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.slayway.slaycloud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
