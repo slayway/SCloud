447cf75aef38a72110d4d25eda5766a1d1667194
slayway <Relocation1337@gmail.com>
2018-09-19 07:32:41 +0300

    added the method of adding to the database

--- Software/app/src/main/java/com/slayway/slaycloud/RegisterActivity.java
+++ Software/app/src/main/java/com/slayway/slaycloud/RegisterActivity.java
@@ -1,5 +1,9 @@
 package com.slayway.slaycloud;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.strictmode.SqliteObjectLeakedViolation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +18,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     EditText etEmail;
     EditText etLogin;
     EditText etPass;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +31,33 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         etEmail = (EditText) findViewById(R.id.editTextEmail);
         etLogin = (EditText) findViewById(R.id.editTextLogin);
         etPass = (EditText) findViewById(R.id.editTextPass);
-
+        dbHelper = new DBHelper(this);
         creaateAcc.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        String firstName = etFirstName.getText().toString();
+        String lastName = etLastName.getText().toString();
+        String email = etEmail.getText().toString();
+        String login = etLogin.getText().toString();
+        String password = etPass.getText().toString();
         switch (view.getId()){
             case R.id.buttonCreateAcc:
+
+                contentValues.put(dbHelper.KEY_FIRSTNAME, firstName);
+                contentValues.put(dbHelper.KEY_LASTNAME, lastName);
+                contentValues.put(dbHelper.KEY_MAIL, email);
+                contentValues.put(dbHelper.KEY_LOGIN, login);
+                contentValues.put(dbHelper.KEY_PASSWORD, password);
+
+                database.insert(dbHelper.TABLE_USERS, null, contentValues);
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
                 break;
 
             default:
